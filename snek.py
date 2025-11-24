bdd6a244ba5ebe82b4a515885354caf44d2e6b36
Skai Sloan <skai.sloan@gmail.com>
2025-11-24 18:17:16 +0000

    basic functionality in snek.py

--- snek.py
+++ snek.py
@@ -1,3 +1,55 @@
 import asyncio
+import time
+import random
+
+# initiate variables
+foodPos = (random.randint(1, 20), random.randint(1, 20))
+snek = {
+    length = 3
+    ate = False
+
+        }
+
+# create the board
+board = (
+"|‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|\r\n"
+"||\r\n"
+"||\r\n"
+"||\r\n"
+"||\r\n"
+"||\r\n"
+"||\r\n"
+"||\r\n"
+"||\r\n"
+"||\r\n"
+"||\r\n"
+"||\r\n"
+"||\r\n"
+"||\r\n"
+"||\r\n"
+"||\r\n"
+"||\r\n"
+"||\r\n"
+"||\r\n"
+"||\r\n"
+"||\r\n"
+"|_____________________________________________|\r\n"
+)
+# process board
+lines = board.splitlines()
+innerWidth = len(lines[0]) - 2  # subtract the two border chars
+boardMid = ["|" + (" " * innerWidth) + "|" for _ in range(20)]
+board = [lines[0]] + boardMid + [lines[-1]]
+board = "\r\n".join(board)
+
+# function to update the board
+def updateBoard():
+    if snek.ate:
+        foodPos = (random.randint(1, 45), random.randint(1, 20)
+
+# main function
 async def main(writer, reader):
-    writer.write("Hello!!\r\n")
+    while True:
+        writer.write(board)
+        time.sleep(0.05)
+        updateBoard()
